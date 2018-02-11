94da8f91ba44349e6975d5f44abc4ac9435d0e70
Gregor Gresak <design@gresak.net>
2018-02-11 21:43:02 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+var gulp 		= require('gulp');
+var less		= require('gulp-less');
+var webserver 	= require('gulp-webserver');
+var plumber		= require('gulp-plumber');
+var concat 		= require('gulp-concat');
+var cleancss 	= require('gulp-clean-css');
+var rename 		= require('gulp-rename');
+var jshint		= require('gulp-jshint');
+var uglify		= require('gulp-ugilfy');
+
+
+gulp.task('less', function() {
+	return gulp.src('./src/less/index.less')
+	.pipe(plumber())
+	/*.pipe(less({
+		paths: [path.join(__dirname,'./node_modules/font-awesome/less/'),
+				path.join(__dirname,'./node_modules/uikit/src/less/')]
+	}))*/
+	.pipe(gulp.dest('./css'))
+	.pipe(concat('styles.css'))
+	.pipe(cleancss())
+	.pipe(rename({
+		suffix:'.min'
+	}))
+	.pipe(gulp.dest('./css'))
+	;
+});
+
+gulp.task('scripts', function() {
+	return gulp.src([
+		'./src/js/*.js'
+		])
+	.pipe(jshint())
+	.pipe(concat('all.js'))
+	.pipe(gulp.dest('./js'))
+	.pipe(rename('all.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('./js'))
+	;
+})
+
+gulp.task('webserver', function() {
+	gulp.src("./")
+	.pipe(webserver({
+		livereload: true,
+		directoryListing: true
+	}))
+	;
+});
+
+gulp.task('watch',function(){
+	gulp.watch('src/js/*.js',['scripts']);
+	gulp.watch('src/less/*.less',['less']);
+})
+
+gulp.task('default',['scripts','less','webserver','watch'])
